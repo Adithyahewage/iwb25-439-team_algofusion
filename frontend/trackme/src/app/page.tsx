82ff499d9d698baf4249496ea6c21faabb0d26b2
Adithya Hewage <adithyahewage69@gmail.com>
2025-08-10 13:44:58 +0530

    small change.

--- frontend/trackme/src/app/page.tsx
+++ frontend/trackme/src/app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Hello World</h1>
+    </div>
+    
   );
 }
